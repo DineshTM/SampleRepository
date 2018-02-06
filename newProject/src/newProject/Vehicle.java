c961ad4e773f2b646e8a1c0dfb41672d8cafdc95
User <User@FULL308-WIN.FULL.Creative>
2018-02-06 14:26:28 +0530

    This is based on string operations

--- newProject/src/newProject/Vehicle.java
+++ newProject/src/newProject/Vehicle.java
@@ -1,10 +1,29 @@
 package newProject;
 
+import java.util.*;
+
 public class Vehicle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Vehicle vehicle = new Vehicle();
+		String vehicleName = new String();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a vehicle name");
+		vehicleName = sc.nextLine();
+		vehicle.convertCase(vehicleName);
 	}
 
+	public void convertCase(String name) {
+		if (name != null && name.length() != 0) {
+			name = name.trim();
+			System.out.println("This is the name: " + name);
+			System.out.println("This is uppercase of the vehicle name: " + name.toUpperCase());
+			System.out.println("This is lowercase of the vehicle name: " + name.toLowerCase());
+			System.out.println("This is last letter of the vehicle name: " + name.charAt(name.length() - 1));
+		}else {
+			System.out.println("INVALID!!! You didnt enter a valid vehicle name ");
+		}
+
+	}
 }
